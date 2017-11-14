0d59da804613da1110b279ad8fff0c8c09a10f77
aleclarson <alec.stanford.larson@gmail.com>
2017-11-14 12:38:20 -0500

    1.1.1

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 
-var assertType = require('assertType');
-
 // Use a native promise to schedule functions. https://goo.gl/fuwiHB
-var promise = Promise.resolve();
+var resolved = Promise.resolve()
 
 // This supports an optional `context` as the first argument.
-module.exports = function immediate(context, callback) {
+module.exports = function immediate(callback) {
   if (arguments.length > 1) {
-    assertType(callback, Function);
-    promise.then(callback.bind(context));
-  } else {
-    callback = context;
-    assertType(callback, Function);
-    promise.then(callback);
+    var context = arguments[0]
+    callback = arguments[1]
   }
-};
+  if (typeof callback != 'function') {
+    throw TypeError('Expected a function')
+  }
+  resolved.then(function() {
+    try {
+      if (context) {
+        callback.call(context)
+      } else {
+        callback()
+      }
+    } catch(error) {
+      console.error(error)
+    }
+  })
+}
